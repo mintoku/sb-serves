190efdfe04ce61effb111559c5385c9560bf6d13
mintoku <jovialow23@gmail.com>
2025-12-18 15:56:23 +0800

    fixed footer formatting

--- app/components/nav/SiteFooter.tsx
+++ app/components/nav/SiteFooter.tsx
@@ -7,13 +7,13 @@ export default function SiteFooter() {
       <div className="mx-8 px-4 py-8">
         <div className="flex flex-col items-center justify-between gap-4 sm:flex-row">
           {/* left */}
-          <p className="order-2 md:order-1 text-sm text-neutral-600">
+          <p className="order-2 sm:order-1 text-sm text-neutral-600">
             © {new Date().getFullYear()} sb serves
           </p>
 
           {/* right (links) */}
           
-          <nav className="grid grid-rows-2 order-1 md:order-2 flex items-center gap-4 mb-2 text-sm">
+          <nav className="grid grid-rows-2 order-1 sm:order-2 flex items-center gap-4 mb-2 text-sm">
             <Link
               href="/signin"
               className="hover:text-blue-800 hover:bg-blue-100 text-center p-3 bg-blue-50 rounded-lg"
